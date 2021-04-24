f1186889bc1fafcdfa53a1dc6b2a5cd7b23a1f3b
awexxx <alex@plaguecraft.xyz>
2021-04-23 21:26:35 -0400

    Big boi updates
    Check the API updates page for more information

--- production.js
+++ production.js
@@ -1,3 +1,13 @@
+///////////////////////////////
+//  PLAGUECRAFT NETWORK API  //
+///////////////////////////////
+
+///////////////////////////////////////////////////////////////////////////////////////
+// This project has been built by me as a part of the PlagueCraft Network Web Force. //
+// It is licensed under the MIT Open-Source License. Check out LICENSE to read more. //
+// The PlagueCraft Network, 2020-2021                                                //
+///////////////////////////////////////////////////////////////////////////////////////
+
 // Defining modules
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -9,15 +19,17 @@ const favicon = require('serve-favicon');
 const https = require('https');
 const http = require('http');
 const morgan = require('morgan')
+const rateLimit = require('express-rate-limit');
 app.use(bodyParser.json());
 app.use(cors());
+app.use(morgan('combined'))
 
 // Error Handler
 process.on('uncaughtException', (error)  => {
     console.log('Oh my god, something terrible happened: ',  error);
 })
 
-//Favicon Definition
+// Favicon Definition
 app.get('/logo.png', (req, res) => res.status(204));
 app.use(favicon(('logo.png')))
 
@@ -30,7 +42,12 @@ const pool = mysql.createPool({
   database        : ''
 });
 
-app.use(morgan('combined'))
+const req = rateLimit({                                                                                              
+  windowMs: 60 * 60 * 1000, // 1 hour window                                                                      
+  max: 100, // start blocking after 100 requests                                                                   
+  message:                                                                                                         
+    {"status": "429 TOO MANY REQUESTS", "message": "You have been rate limited."}          
+});
 
 // / endpoint
 app.get('/', (req, res) => {
@@ -44,7 +61,7 @@ app.get('/v0',(req, res) => {
   });
 
   // List all econ data
-app.get('/v0/smp/data',(req, res) => {
+app.get('/v0/smp',(req, res) => {
   let sql = "SELECT * FROM xconomy"; // SQL Query
   let query = pool.query(sql, (err, results) => { // Run the query
     if(err) {
@@ -57,7 +74,7 @@ app.get('/v0/smp/data',(req, res) => {
 
 // List only player name and bal (specifically for the PCN Bot)
 app.get('/v0/smp/bal/:id',(req, res) => {
-    let sql = "SELECT balance, player FROM xconomy WHERE player="+req.params.id;
+    let sql = `SELECT balance, player FROM xconomy WHERE player = '${req.params.id}'`;
     let query = pool.query(sql, (err, results) => {
         if(err) {
             res.status(404).send(JSON.stringify({"status": "200 OK", "error": "404 NOT FOUND", "message": "The requested resource was not found. If you expected something to be here, contact the owner of the application (PCN)"}));
@@ -69,7 +86,7 @@ app.get('/v0/smp/bal/:id',(req, res) => {
 
 // List all data by player
 app.get('/v0/smp/:id',(req, res) => {
-  let sql = 'SELECT * FROM xconomy WHERE player = ' + req.params.id;
+  let sql = `SELECT * FROM xconomy WHERE player = '${req.params.id}'`;
   let query = pool.query(sql, (err, results) => {
     if(err) {
         res.status(404).send(JSON.stringify({"status": "200 OK", "error": "404 NOT FOUND", "message": "The requested resource was not found. If you expected something to be here, contact the owner of the application (PCN)"}));
@@ -80,7 +97,7 @@ app.get('/v0/smp/:id',(req, res) => {
 });
 
 // List all SkyWars data
-  app.get('/v0/sw/data',(req, res) => {
+  app.get('/v0/sw',(req, res) => {
     let sql = "SELECT * FROM sw_player";
     let query = pool.query(sql, (err, results) => {
         if(err) {
@@ -92,8 +109,8 @@ app.get('/v0/smp/:id',(req, res) => {
   });
 
   // Get SkyWars data by player name
-  app.get('/v0/sw/:id',(req, res) => {
-    let sql = "SELECT * FROM sw_player WHERE player_name = "+req.params.id;
+  app.get(`/v0/sw/:id`,(req, res) => {
+    let sql = `SELECT * FROM sw_player WHERE player_name = '${req.params.id}'`;
     let query = pool.query(sql, (err, results) => {
         if(err) {
             res.status(404).send(JSON.stringify({"status": "200 OK", "error": "404 NOT FOUND", "message": "The requested resource was not found. If you expected something to be here, contact the owner of the application (PCN)"}));
@@ -103,26 +120,20 @@ app.get('/v0/smp/:id',(req, res) => {
     });
   });
 
-  app.get('*', function(req, res){
-       res.send(JSON.stringify({"status": "200 OK", "error": "404 NOT FOUND", "message": "The requested resource was not found. If you expected something to be here, contact the owner of the application (PCN)"}));
-  });
-
   // Spin up a simple Express HTTP server
 const httpServer = http.createServer(app);
 
-// Spin up an HTTPS server
-const httpsServer = https.createServer({
-  key: fs.readFileSync('/etc/letsencrypt/live/api.plaguecraft.xyz/privkey.pem'),
-  cert: fs.readFileSync('/etc/letsencrypt/live/api.plaguecraft.xyz/fullchain.pem')
-}, app);
+const httpsServer = https.createServer({                                                                             
+  key: fs.readFileSync('/etc/letsencrypt/live/api.plaguecraft.xyz/privkey.pem'),                                     
+  cert: fs.readFileSync('/etc/letsencrypt/live/api.plaguecraft.xyz/fullchain.pem')                                   
+}, app); 
 
 // Log Enable
 httpServer.listen(80, () => {
-    console.log('Production API now running on port 80, redirecting to 443');
 });
 
-httpsServer.listen(443, () => {
-  console.log('Production API now running on port 443');
-  console.log(`PCNAPI -- Production Mode Online.`);
-})
+httpsServer.listen(443, () => {                                                                                      
+  console.log('Production API now running on port 443');                                                             
+  console.log(`PCNAPI -- Production Mode Online.`);                                                                  
+})  
 
