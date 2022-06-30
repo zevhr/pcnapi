a5cf9378d42dcd37d80845877775cddec5b7bd4f
Alex <hi@thatalex.dev>
2022-06-30 19:56:02 -0400

    refactor: add plugin endpoint, add wss

--- src/modules/redis.ts
+++ src/modules/redis.ts
@@ -1,5 +1,5 @@
 import { createClient } from 'redis';
 import config from '../config.json';
 
-export const redisClient = createClient({url: config.redis.url});
+export const redisClient = createClient({ url: config.redis.url });
 redisClient.connect();
--- src/routes/plugins.ts
+++ src/routes/plugins.ts
@@ -6,7 +6,7 @@ import fetch from 'node-fetch';
 export const pluginRoute = Router();
 
 pluginRoute.get('/plugins/:plugin', (req, res) => {
-    const set = new Set(["bridges","tntrun","proxyif"])
+    const set = new Set(["bridges","tntrun","parkour","proxyif"]);
     if (set.has(req.params.plugin)) {
         fetch("https://api.github.com/repos/plaguecraft-team/" + req.params.plugin.toLowerCase() + "/releases/latest", {
             method: 'get',
@@ -14,16 +14,17 @@ pluginRoute.get('/plugins/:plugin', (req, res) => {
                 'authorization': 'token ' + config.github
             }
         }).then(async (response) => {
-            const j = await response.json();
-
             if (response.status === 200) {
+                const j = await response.json();
                 return res.status(200).json({
-                    "status": 200,
-                    "latest_file": j.assets[0].name,
-                    "last_updated": j.assets[0].created_at,
-                    "download": j.assets[0].browser_download_url
+                    "status":200,
+                    "file": {
+                        "latest_version": j.tag_name,
+                        "assets_url": j.assets_url,
+                        "file_name": j.assets[0].name
+                    }
                 })
-            } else return res.status(400).json({"status":400,"message":"An unknown error occurred. It's not you, it's us."})
+            } else return res.status(500).json({"status":500,"message":"It's not you, it's us. GitHub's API returned an unexpected error."});
         })
-    } else return res.status(404).json({"status":404,"message":"You've queried for an improper plugin.", "expected": set})
+    } else return res.status(404).json({"status":404,"message":"An unrecognized plugin was provided"});
 })
